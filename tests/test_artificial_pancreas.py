ddad58db32be0c42fe817dbb12d301d2e4176ac4
Hezekiah Ajayi-Omoleye <hezekiahomoleye@gmail.com>
2025-10-21 17:57:09 +0100

    Testings

--- tests/test_artificial_pancreas.py
+++ tests/test_artificial_pancreas.py
@@ -1,10 +1,25 @@
-import unittest 
-import main.artificial_pancreas as artificial_pancreas
+import pytest
+from  main.artificial_pancreas import  ArtificialPancreasSystem
 
-class TestArtificialPancreas(unittest.TestCase):
-    def test_calculate_insulin_dosage(self):
-        aps = artificial_pancreas.ArtificialPancreasSystem(glucose_level=180, insulin_sensitivity=0.1, target_glucose=120, tolerance=10)
-        insulin_dosage = aps.insulin_sensitivity * (aps.glucoselevel - aps.target_glucose)
-        self.assertEqual(insulin_dosage, 6.0)  # 0.1 * (180 - 120) = 6.0
 
-unittest.main()
+@pytest.fixture
+def instance():
+    return ArtificialPancreasSystem(glucose_level=100, target_glucose=10, tolerance=0.1, insulin_sensitivity=10)
+
+
+
+def test_meal(instance):
+    start = instance.glucose_level
+   # pan = instance.meal(carbs=60)
+   # assert instance.glucose_level > start
+   # assert pan > start, f"Expects pan to be greater than start"
+    
+def test_exercise(instance):
+    exp = instance.glucose_level
+    dur = instance.exercise(duration=40)
+    assert exp > dur                        # You can't compare an int and None
+
+#def test_predict_action(var):
+ #   aps = ArtificialPancreasSystem(insulin_sensitivity = 0.1, target_glucose =120, tolerance = 10, glucose_level = 5.0)
+  #  action, level = var.pretict_action
+# Read more on type hints in pyton
